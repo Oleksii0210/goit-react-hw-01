f07cb10e51baa97e43089ef29288681d3befc072
Oleksii0210 <141589602+Oleksii0210@users.noreply.github.com>
2024-05-26 15:25:11 +0300

    new

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,10 +4,9 @@ export default function Profile({ username, tag, location, avatar, stats }) {
         <div className={css.card}>
         <div className={css.cardInfo}>
             <img className={css.cardImg}
-         src={image}
-                    alt="User avatar"
+             src={avatar} alt="User avatar"
          />
-         <p className={css.nameText}>{name}</p>
+         <p className={css.nameText}>{username}</p>
                 <p className={css.tagText}>@{tag}</p>
                 <p className={css.tagText}>{location}</p>
          </div>
