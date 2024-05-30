95fbcdbdbd598ca3637dd050f60e2b1764b20323
Oleksii0210 <141589602+Oleksii0210@users.noreply.github.com>
2024-05-30 16:41:16 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-import Profile from './components/Profile/Profile'
-import userData from './userData.json'
-
+import Profile from "./components/Profile/Profile";
+import userData from "./userData.json";
+import FriendList from "./components/FriendList/FriendList";
+import friends from "./friends.json";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import transactions from "./transactions.json";
 
 const App = () => {
   return (
@@ -12,6 +15,8 @@ const App = () => {
         avatar={userData.avatar}
         stats={userData.stats}
       />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
